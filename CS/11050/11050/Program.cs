b44bfd757296fac5cd996bd3cc7eb1d52bfd0360
Jung-Haewon <speedhaewon@naver.com>
2021-10-04 05:12:52 +0900

    2021-10-03 C# 한문제

---
+++ CS/11050/11050/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+            int ans = 1;
+
+            for(int i = 0; i < num[1]; i++)
+            {
+                ans *= num[0];
+                num[0]--;
+            }
+
+            for (int i = 0; i < num[1]; i++)
+            {
+                ans /= (i + 1);
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
