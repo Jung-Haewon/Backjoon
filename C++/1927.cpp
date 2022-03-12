70707e07c7b5130c580d859bde2ca9c3efb7e513
Jung-Haewon <62277506+Jung-Haewon@users.noreply.github.com>
2022-03-13 00:21:09 +0900

    2022-03-12
    우선순위 큐

--- C++/1927.cpp
+++ C++/1927.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <queue>
+#include <string>
 using namespace std;
 
 int main(void)
 {
-	int n;
+	priority_queue<int, vector<int>, greater<int>> arr;
+	int n, x;
+	string ans = "";
 
+	scanf("%d", &n);
+
+	for(int i = 0; i < n; i++)
+	{
+		scanf("%d", &x);
+
+		if(x > 0)
+			arr.push(x);
+		else
+		{
+			if(arr.empty() == true)
+				ans += "0\n";
+			else
+			{
+				ans += to_string(arr.top()) + "\n";
+				arr.pop();
+			}
+		}
+	}
+
+	cout << ans;
 
 	return 0;
 }
